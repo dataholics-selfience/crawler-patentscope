8c92b5629b24afa24363d200d646890bbe582ae5
dataholics-selfience <48730751+dataholics-selfience@users.noreply.github.com>
2025-10-10 15:34:59 -0300

    Create playwrightCrawler.js

---
+++ src/crawlers/playwrightCrawler.js
@@ -0,0 +1,5 @@
+// src/crawlers/playwrightCrawler.js
+export async function playwrightCrawler(medicine) {
+  console.log("playwrightCrawler chamado com", medicine);
+  return "<html><body>Stub Playwright HTML</body></html>";
+}
