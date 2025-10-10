01cf2252736d934e0c2cedae1ecadf81478f7f53
dataholics-selfience <48730751+dataholics-selfience@users.noreply.github.com>
2025-10-10 15:35:40 -0300

    Create seleniumCrawler.js

---
+++ src/crawlers/seleniumCrawler.js
@@ -0,0 +1,5 @@
+// src/crawlers/seleniumCrawler.js
+export async function seleniumCrawler(medicine) {
+  console.log("seleniumCrawler chamado com", medicine);
+  return "<html><body>Stub Selenium HTML</body></html>";
+}
